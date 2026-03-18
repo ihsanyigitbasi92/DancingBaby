4b11a212aa5de9a83a8c6730e1a3508b3dc4e7db
ihsanyigitbasi92 <ihsanyigitbasi92@gmail.com>
2026-03-19 00:52:44 +0400

    Add mobile_game/app/animation.py

---
+++ mobile_game/app/animation.py
@@ -0,0 +1,14 @@
+import pygame
+
+class Animation:
+    def __init__(self):
+        self.speed = 1
+
+    def start_dancing(self):
+        # Load and start the dancing animation
+        pass
+
+    def increase_speed(self):
+        self.speed += 0.1
+        # Increase animation speed
+        pass
